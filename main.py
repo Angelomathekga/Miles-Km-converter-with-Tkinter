e878422ba9c74406e1df2a83dbb1da0d4aeb40c3
angelo <angelomathekga@gmail.com>
2022-10-15 20:19:34 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,117 @@
+from tkinter import *
+
+# #Creating a new window and configurations
+# window = Tk()
+# window.title("Widget Examples")
+# window.minsize(width=500, height=500)
+#
+# #Labels
+# label = Label(text="This is old text")
+# label.config(text="This is new text")
+# label.pack()
+#
+# #Buttons
+# def action():
+#     print("Do something")
+#
+# #calls action() when pressed
+# button = Button(text="Click Me", command=action)
+# button.pack()
+#
+# #Entries
+# entry = Entry(width=30)
+# #Add some text to begin with
+# entry.insert(END, string="Some text to begin with.")
+# #Gets text in entry
+# print(entry.get())
+# entry.pack()
+#
+# #Text
+# text = Text(height=5, width=30)
+# #Puts cursor in textbox.
+# text.focus()
+# #Adds some text to begin with.
+# text.insert(END, "Example of multi-line text entry.")
+# #Get's current value in textbox at line 1, character 0
+# print(text.get("1.0", END))
+# text.pack()
+#
+# #Spinbox
+# def spinbox_used():
+#     #gets the current value in spinbox.
+#     print(spinbox.get())
+# spinbox = Spinbox(from_=0, to=10, width=5, command=spinbox_used)
+# spinbox.pack()
+#
+# #Scale
+# #Called with current scale value.
+# def scale_used(value):
+#     print(value)
+# scale = Scale(from_=0, to=100, command=scale_used)
+# scale.pack()
+#
+# #Checkbutton
+# def checkbutton_used():
+#     #Prints 1 if On button checked, otherwise 0.
+#     print(checked_state.get())
+# #variable to hold on to checked state, 0 is off, 1 is on.
+# checked_state = IntVar()
+# checkbutton = Checkbutton(text="Is On?", variable=checked_state, command=checkbutton_used)
+# checked_state.get()
+# checkbutton.pack()
+#
+# #Radiobutton
+# def radio_used():
+#     print(radio_state.get())
+# #Variable to hold on to which radio button value is checked.
+# radio_state = IntVar()
+# radiobutton1 = Radiobutton(text="Option1", value=1, variable=radio_state, command=radio_used)
+# radiobutton2 = Radiobutton(text="Option2", value=2, variable=radio_state, command=radio_used)
+# radiobutton1.pack()
+# radiobutton2.pack()
+#
+#
+# #Listbox
+# def listbox_used(event):
+#     # Gets current selection from listbox
+#     print(listbox.get(listbox.curselection()))
+#
+# listbox = Listbox(height=4)
+# fruits = ["Apple", "Pear", "Orange", "Banana"]
+# for item in fruits:
+#     listbox.insert(fruits.index(item), item)
+# listbox.bind("<<ListboxSelect>>", listbox_used)
+# listbox.pack()
+# window.mainloop()
+
+window = Tk()
+window.title("Mile to Km converter")
+window.config(padx=20, pady=20)
+
+
+def calculate():
+    miles = float(entry_1.get())
+    km = miles * 1.609
+    label_3.config(text=f"{km}")
+
+
+
+label_1 = Label(text="Miles")
+label_1.grid(column= 2, row=0)
+
+label_2 = Label(text="Is equal to:")
+label_2.grid(column=0, row=1)
+
+label_3 = Label(text="Answer")
+label_3.grid(column=1, row=1)
+
+label_4 = Label(text="Km")
+label_4.grid(column=2, row=1)
+
+entry_1 = Entry(width=15)
+entry_1.grid(column=1, row=0)
+
+button_1 = Button(text="Calculate", width=14, command=calculate)
+button_1.grid(column=1, row=2)
+
+window.mainloop()
